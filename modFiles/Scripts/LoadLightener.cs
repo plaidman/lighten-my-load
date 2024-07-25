592ac0ce843a4cc6c51f371d1947762a9d18f907
jason tomsic <jrtomsic@gmail.com>
2024-07-25 15:07:15 -0400

    keeping track of known items

---
+++ modFiles/Scripts/LoadLightener.cs
@@ -0,0 +1,124 @@
+using System;
+using System.Collections.Generic;
+using XRL.UI;
+
+// TODOs
+// show list of all inventory items
+// show weight in list
+// sort list by weight OF EACH ITEM
+// 
+// drop selected items
+//
+// calculate sell value for items
+// exclude yellow items from calculation
+// generate ratio label
+// show ratio label in list
+// sort item list by value ratio
+// show questionmark as ratio label if not in known items list
+// 
+// key to change sort mode in list
+//
+// create skill
+// show all ratio labels if you have the skill
+// figure out how to check if PKAPP skill
+// show all values if you have PKAPP skill
+
+namespace XRL.World.Parts {
+	[Serializable]
+	public class LML_LoadLightener : IPlayerPart {
+		public static readonly string ItemListCommand = "Plaidman_LightenMyLoad_Command_ShowItemList";
+		public static readonly string UninstallCommand = "Plaidman_LightenMyLoad_Command_Uninstall";
+		public static readonly string ShowValueOption = "Plaidman_LightenMyLoad_Option_AlwaysShowValue";
+		public static readonly string AbilityOption = "Plaidman_LightenMyLoad_Option_UseAbility";
+		public Guid AbilityGuid;
+		public HashSet<string> KnownItems;
+		
+		public LML_LoadLightener() {
+			KnownItems = new(50);
+		}
+
+        public override void Register(GameObject go, IEventRegistrar registrar) {
+			registrar.Register(CommandEvent.ID);
+			registrar.Register(AfterPlayerBodyChangeEvent.ID);
+			registrar.Register(StartTradeEvent.ID);
+            base.Register(go, registrar);
+        }
+
+		public void ToggleAbility() {
+			if (Options.GetOption(AbilityOption) == "Yes") {
+				RequireAbility();
+			} else {
+				RemoveAbility();
+			}
+		}
+
+		private void RequireAbility() {
+			if (AbilityGuid == Guid.Empty) {
+				AbilityGuid = ParentObject.AddActivatedAbility("Lighten Load", ItemListCommand, "Skill", Silent: true);
+			}
+		}
+	
+		private void RemoveAbility() {
+			if (AbilityGuid != Guid.Empty) {
+				ParentObject.RemoveActivatedAbility(ref AbilityGuid);
+			}
+		}
+
+		public override bool HandleEvent(AfterPlayerBodyChangeEvent e) {
+			ToggleAbility();
+            return base.HandleEvent(e);
+        }
+
+        public override bool HandleEvent(StartTradeEvent e) {
+			if (!e.Trader.IsCreature) {
+				return base.HandleEvent(e);
+			}
+			
+			foreach (var item in ParentObject.Inventory.GetObjects()) {
+				KnownItems.Add(item.BaseDisplayName);
+			}
+
+			return base.HandleEvent(e);
+        }
+
+		private void UninstallParts() {
+			if (Popup.ShowYesNo("Are you sure you want to uninstall {{W|Lighten My Load}}?") == DialogResult.No) {
+				Messages.MessageQueue.AddPlayerMessage("{{W|Lighten My Load}} uninstall was cancelled.");
+				return;
+			}
+
+			if (AbilityGuid != Guid.Empty) {
+				ParentObject.RemoveActivatedAbility(ref AbilityGuid);
+				Messages.MessageQueue.AddPlayerMessage("{{W|Lighten My Load}}: removed ability");
+			}
+
+			ParentObject.RemovePart<LML_LoadLightener>();
+			Messages.MessageQueue.AddPlayerMessage("{{W|Lighten My Load}}: removed player part");
+			
+			Popup.Show("Finished removing {{W|Lighten My Load}}. Please save and quit, then you can remove this mod.");
+		}
+
+        public override bool HandleEvent(CommandEvent e) {
+			if (e.Command == ItemListCommand) {
+				ListItems();
+			}
+
+			if (e.Command == UninstallCommand) {
+				UninstallParts();
+			}
+
+			return base.HandleEvent(e);
+		}
+		
+		private void ListItems() {
+			var message = "known items:\n";
+			foreach (var item in KnownItems) {
+				message += item + "\n";
+			}
+			
+			message += "(end " + KnownItems.Count + ")";
+
+			Popup.Show(message);
+		}
+	}
+}
